b8bbf93118dc792afe779695254e8afdced10a6a
moonjin-kim <ace39418@gmail.com>
2023-10-23 23:33:27 +0900

    test : prototype bean test

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,39 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+    @Test
+    void prototypeBeanFind(){
+        //given
+        AnnotationConfigApplicationContext ac =new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("find prototypeBean12");
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+        Assertions.assertThat(bean1).isNotSameAs(bean2);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("PrototypeBean.close");
+        }
+    }
+}
